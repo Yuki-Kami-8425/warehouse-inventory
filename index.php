2f66bfc8d5b979ba9d0e1fbd75d3dc8b0c5a0ece
Yuki-Kami-8425 <kyumayuki@gmail.com>
2024-11-04 12:54:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -18,13 +18,13 @@ if ($conn === false) {
 // Lấy trạm từ query string, mặc định là 'home'
 $station = isset($_GET['station']) ? $_GET['station'] : 'home';
 
-// Lấy dữ liệu từ bảng cho tất cả các trạm nếu là 'all', ngược lại lấy theo trạm
+/* // Lấy dữ liệu từ bảng cho tất cả các trạm nếu là 'all', ngược lại lấy theo trạm
 if ($station === 'all') {
     $sql = "SELECT MAKH, TENKH, LUONG_PALLET, RFID FROM dbo.stored_warehouse";
 } else {
     $sql = "SELECT MAKH, TENKH, LUONG_PALLET, RFID FROM dbo.stored_warehouse WHERE RFID LIKE ?";
     $params = array($station . '%');
-}
+} */
 $stmt = sqlsrv_query($conn, $sql, $params ?? null); 
 
 // Kiểm tra lỗi khi truy vấn
