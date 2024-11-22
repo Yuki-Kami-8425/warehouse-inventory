cdae8224f30676ef14a8e2c8b4aaaca21cca0915
Yuki-Kami-8425 <kyumayuki@gmail.com>
2024-11-22 15:59:02 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ case 'home':
     $sql = null; // Hoặc không cần khởi tạo $sql
     break;
 case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
-    $sql = "SELECT MAKH, TENSP, TENKH, LUONG_PALLET, RFID FROM dbo.stored_warehouse WHERE RFID LIKE ?";
+    $sql = "SELECT NGAYCT, MAKH, TENSP, TENKH, LUONG_PALLET, RFID FROM dbo.stored_warehouse WHERE RFID LIKE ?";
     $params = array($station . '%');
     break;
 default:
