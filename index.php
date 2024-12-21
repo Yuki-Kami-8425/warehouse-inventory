f5d32bedba7600d6958930c99c2a8c82f6d17a8d
Yuki-Kami-8425 <kyumayuki@gmail.com>
2024-12-21 21:34:12 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -88,8 +88,9 @@ sqlsrv_close($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Warehouse Management - <?= $station === 'all' ? 'All Stations' : 'Station ' . $station ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-piechart-outlabels"></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-piechart-outlabels"></script>
 
     <script>
         window.onload = function() { // Kiểm tra xem URL có chứa tham số station không
